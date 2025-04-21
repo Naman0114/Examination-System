28ed34ad26fa132965a9554b92e21dab29ea2421
ZAFARAHMAD01 <za897192@gmail.com>
2025-04-21 11:28:00 +0530

    next commit

--- server/config/db.js
+++ server/config/db.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 // mongodb://localhost:27017/test1
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb+srv://zafarahmad:2a2q8mOQuvJg44ll@cluster0.zffii.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/test1",);
+    const conn = await mongoose.connect("mongodb+srv://zafarahmad:2a2q8mOQuvJg44ll@cluster0.zffii.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",);
     console.log(`MongoDB Connected: {conn.connection.host}`);
   } catch (error) {
     console.error(error.message);
