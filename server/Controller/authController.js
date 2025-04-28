585a31367d2d3a9f6a66c1575997f9772728c8fe
Naman0114 <namanprabhatchaturvedi0114@gmail.com>
2025-04-29 00:04:32 +0530

    commit

--- server/Controller/authController.js
+++ server/Controller/authController.js
@@ -10,8 +10,9 @@ async function authController(req,res,next){
         throw new Error("Signature is invalid");
 
     }
-    const recoveredAddress=ethers.utils.verifyMessage("Welcome to Authenticate Wallet",signature);
-    console.log(recoveredAddress);
+    const recoveredAddress = ethers.utils.verifyMessage("Welcome to Authenticate Wallet", signature);
+
+    console.log("Recovered address",recoveredAddress);
 
     if(address.toLowerCase()===recoveredAddress.toLowerCase()){
         res.status(200).json({message:"Authentication Successful"});
